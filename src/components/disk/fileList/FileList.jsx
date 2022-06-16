a419f36847310ddbdcc9ff84635893ea25b8f889
AI-Alert <50011985+AI-Alert@users.noreply.github.com>
2022-06-16 11:13:16 +0200

    Adding download function

--- src/components/disk/fileList/FileList.jsx
+++ src/components/disk/fileList/FileList.jsx
@@ -5,7 +5,7 @@ import File from "./file/File";
 
 const FileList = () => {
 
-    const files = useSelector(state => state.files.files).map(file => <File key={file.id} file={file}/>)
+    const files = useSelector(state => state.files.files).map(file => <File key={file._id} file={file}/>)
 
     return (
         <div className='filelist'>
@@ -20,3 +20,8 @@ const FileList = () => {
 };
 
 export default FileList;
+
+
+
+
+

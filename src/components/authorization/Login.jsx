a3c63d08ecfa9fe4249d01ba40d7bd6b26ab701e
AI-Alert <50011985+AI-Alert@users.noreply.github.com>
2022-06-17 13:48:57 +0200

    View corrections (Frontend)

--- src/components/authorization/Login.jsx
+++ src/components/authorization/Login.jsx
@@ -4,12 +4,19 @@ import Input from "../../utils/input/Input";
 import {useDispatch} from "react-redux";
 import {login} from "../../actions/user";
 
+//Компонент авторизации
+
+//Функция Login
 const Login = () => {
+    //инициализация стейта, содержащего email
     const [email, setEmail] = useState("")
+    //инициализация стейта, содержащего пароль
     const [password, setPassword] = useState("")
+    //инициализация переменной dispatch, которая включает в себя использование Dispatch
     const dispatch = useDispatch()
 
     return (
+        //вывод формы входа с email и паролем
         <div className='authorization'>
             <div className="authorization__header">Authorization</div>
             <Input value={email} setValue={setEmail} type="text" placeholder="Enter email..."/>
--- src/components/authorization/Registration.jsx
+++ src/components/authorization/Registration.jsx
@@ -4,10 +4,13 @@ import Input from "../../utils/input/Input";
 import {registration} from "../../actions/user";
 
 const Registration = () => {
+    //инициализация стейта, содержащего email
     const [email, setEmail] = useState("")
+    //инициализация стейта, содержащего пароль
     const [password, setPassword] = useState("")
 
     return (
+        //вывод формы регистрации с email и паролем
         <div className='authorization'>
             <div className="authorization__header">Registration</div>
             <Input value={email} setValue={setEmail} type="text" placeholder="Enter email..."/>
